f92a6db79eb4f3e2f23e2c424e3b6263ad63027c
Tim Pohlad-Thomas <timpohladthomas@gmail.com>
2014-08-08 10:08:32 -0400

    Update and rename liquid.mk to ose.mk

--- ose.mk
+++ ose.mk
@@ -1,14 +1,14 @@
 # Inherit some common LS stuff.
-$(call inherit-product, vendor/liquid/config/common_phone.mk)
+$(call inherit-product, vendor/ose/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/ose/config/nfc_enhanced.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 720
 TARGET_SCREEN_HEIGHT := 1280
 
 # Release name
-PRODUCT_NAME := liquid_moto_msm8960_jbbl
+PRODUCT_NAME := ose_moto_msm8960_jbbl
 
 $(call inherit-product, device/motorola/moto_msm8960_jbbl/full_moto_msm8960.mk)
